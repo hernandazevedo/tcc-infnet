7925312dd01edc3d379399dd3e964cbe0d91001d
hernand.azevedo@gmail.com <hernand.azevedo@gmail.com@de7ae671-8aeb-50e3-01c5-0217eb8ec8b7>
2013-03-05 19:06:59 +0000

    Initial import.

---
+++ OsManagerRobot/src/br/com/infnet/os/manager/robot/AlocaOSRobot.java
@@ -0,0 +1,22 @@
+package br.com.infnet.os.manager.robot;
+
+public class AlocaOSRobot {
+	
+	
+	public static void main(String[] args) {
+		
+		//TODO definir o que vira dos args...
+		//Ler as os�s agendadas para o dia.
+		
+		/*
+		 * Ler os tecnicos em espera, se for horario anterior a 8h,
+		 *  e o tecnico estiver em espera despachar 5 Os�s para ele.
+		 */
+		
+		/*
+		 * Se horario for maior que 8h, entao verifica o tempo de atendimento das 
+		 * OSs que estao em campo(com tecnicos) e se o tempo estiver acabando realocar para o melhor tecnico capaz de 
+		 * atende-la e com tempo disponivel. 
+		 */
+	}
+}
